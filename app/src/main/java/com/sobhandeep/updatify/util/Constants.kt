32cd09535b08262f9c296aab43cff9b6acb42670
Sobhandeep Gope <130676586+sobhandeep@users.noreply.github.com>
2024-10-31 01:03:54 +0530

    Initial Commit

--- app/src/main/java/com/sobhandeep/updatify/util/Constants.kt
+++ app/src/main/java/com/sobhandeep/updatify/util/Constants.kt
@@ -6,7 +6,7 @@ object Constants {
 
     const val APP_ENTRY = "appEntry"
 
-    const val API_KEY = "8a1485c172ac46f28ad83b137928146c"
+    const val API_KEY = "YOUR_API_KEY"
 
     const val BASE_URL = "https://newsapi.org/v2/"
 
